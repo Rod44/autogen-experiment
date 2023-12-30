e2846f8325a5cda38a80459f3e68a0af01ac1784
ROD <rodrigue.buisson@partoo.fr>
2023-12-30 17:53:04 +0100

    d

--- content.py
+++ content.py
@@ -14,3 +14,23 @@ from langchain import PromptTemplate
 
 import openai
 from dotenv import load_dotenv
+
+# Get API key
+load_dotenv()
+config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Define research function
+
+def search(query):
+    url = "https://www.google.com/search?q=" + query
+    payload = json.dumps({
+        "q": query
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
+    return response.json()
+
